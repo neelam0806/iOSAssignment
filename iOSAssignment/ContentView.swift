f0dce28dfc9d9da7a8d7d3e1a688484037ed9598
Neelam Gupta <neelam.gupta0806@gmail.com>
2020-02-01 18:03:28 +0530

    Initial Commit

---
+++ iOSAssignment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSAssignment
+//
+//  Created by Neelam Gupta on 01/02/20.
+//  Copyright © 2020 Neelam Gupta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
