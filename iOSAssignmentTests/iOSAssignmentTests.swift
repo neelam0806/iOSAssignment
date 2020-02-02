a30338e201bda7af590ba67273f5e1b4d779a57b
Neelam Gupta <neelam.gupta0806@gmail.com>
2020-02-02 14:15:05 +0530

    Commit unit test changes

--- iOSAssignmentTests/iOSAssignmentTests.swift
+++ iOSAssignmentTests/iOSAssignmentTests.swift
@@ -15,6 +15,26 @@ class iOSAssignmentTests: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
+    func testJSONData() {
+           guard let jsonUrl = URL(string: "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json") else { return }
+           let promise = expectation(description: "Simple Request")
+           URLSession.shared.dataTask(with: jsonUrl) { (data, response
+               , error) in
+               guard let data = data else { return }
+               do {
+                   let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers)
+                   if let result = json as? NSDictionary {
+                       print("result", result)
+                       XCTAssertTrue(true)
+                       promise.fulfill()
+                   }
+               } catch let err {
+                   print("Err", err)
+               }
+               }.resume()
+           waitForExpectations(timeout: 5, handler: nil)
+       }
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
